740dc7d89150063291d84d2cde41f7802cec4c5e
STD\s20217019 <20217019-emukaikaito@hcs.ac.jp>
2022-10-25 14:01:07 +0900

    コメント追加テスト

--- Form1/Form1/Program.cs
+++ Form1/Form1/Program.cs
@@ -17,6 +17,8 @@ namespace Form1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //コメント追加テスト
         }
     }
 }
