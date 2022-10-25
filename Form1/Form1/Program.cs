92228b5a8f8dc3e0cba1181a9ee492a619f71785
STD\s20217019 <20217019-emukaikaito@hcs.ac.jp>
2022-10-25 14:04:34 +0900

    Revert "コメント追加テスト"
    This reverts commit 740dc7d89150063291d84d2cde41f7802cec4c5e.

--- Form1/Form1/Program.cs
+++ Form1/Form1/Program.cs
@@ -17,8 +17,6 @@ namespace Form1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            //コメント追加テスト
         }
     }
 }
